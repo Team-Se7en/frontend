04738591fb6d7b73fc0f01593a0af6b9b3fdbbc9
Armin Afzali <armin11dse@gmail.com>
2024-04-05 17:04:09 +0330

    feat: Added images + site logo (needs rework).

---
+++ sadFront/src/assets/images/index.ts
@@ -0,0 +1,3 @@
+export { default as AuthBackgroundImgae } from "./auth-background.png"
+export { default as ProfessorImage } from "./professor-background.png"
+export { default as StudentImage } from "./student-background.png"
