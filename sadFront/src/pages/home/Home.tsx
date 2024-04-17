e71ce7adceaaa9ad50d9cf64cf5f676af965d8fa
Arman <arman.zarkalam@gmail.com>
2024-04-17 13:19:54 +0330

    INIT: initializing ProgramModal

--- sadFront/src/pages/home/Home.tsx
+++ sadFront/src/pages/home/Home.tsx
@@ -1,15 +1,16 @@
 import { ProfessorRequestCard } from "@components";
-import { CardModel, Status } from "@models"
+import { CardModel, Status } from "@models";
 import { ProgramsList } from "components/programslist/ProgramsList";
 
 export function Home() {
   const model: CardModel = {
-    title: 'Laboratory Research',
-    description: 'Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab.Need a hardworking student for experiments of living being in my lab.Need a hardworking student for experiments of living being in my lab.Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. ',
-    professorId: '111',
+    title: "Laboratory Research",
+    description:
+      "Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab.Need a hardworking student for experiments of living being in my lab.Need a hardworking student for experiments of living being in my lab.Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. Need a hardworking student for experiments of living being in my lab. ",
+    professorId: "111",
     status: Status.closed,
-    tags: ['science', 'lab']
-  }
+    tags: ["science", "lab"],
+  };
   return (
     <>
       <ProfessorRequestCard model={model}></ProfessorRequestCard>
