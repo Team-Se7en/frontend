0fce83ab4cccf1896c48c2bce805705c7b94243c
AliAlizadeh <alializadeh.dev11@gmail.com>
2024-04-18 15:03:35 +0330

    feat: add edit profile and social media connection to professor home page

--- sadFront/src/pages/home/Professor/ProfessorHomePage-styles.tsx
+++ sadFront/src/pages/home/Professor/ProfessorHomePage-styles.tsx
@@ -51,76 +51,41 @@ const HomePageProfessorStyles = makeStyles(() =>
                 backgroundImage: 'linear-gradient(135deg, #bde0fe 0%, #03045e 250%)',
                 padding: '2rem',
                 position: 'absolute',
-                top: '290px',
+                top: '250px',
                 left: '200px',
+                
 
+        },
+
+        editprofilebutton:{
+            width: '120px',
+            height: '40px',
+            borderRadius: '50%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontWeight: 'bold',
+            position: 'absolute',
+            top: '350px',
+            left: '1200px',
+            padding: '10px 10px',
+
+        },
+
+        socialicons:{
+            width: '100px',
+            height: '40px',
+            display: 'flex',
+            alignItems: 'stretch',
+            justifyContent: 'space-between',
+            position: 'absolute',
+            top: '350px',
+            left: '1000px',
+            padding: '10px',
+            
         }
 
     })
 );
 
 export default HomePageProfessorStyles;
-
-// import { createStyles, makeStyles } from "@mui/styles";
-
-// import { ProfessorHomePage } from "@assets";
-
-// const HomePageProfessorStyles = makeStyles(() =>
-//     createStyles({
-//         paperContainer: {
-//             backgroundImage: `url(${ProfessorHomePage})`,
-//             width: '100%',
-//             height: '50vh',  // Ensuring the container takes full viewport height
-//             backgroundSize: 'cover',
-//             backgroundPosition: 'top',
-//             backgroundRepeat: 'no-repeat',
-//         },
-
-//         positions1: {
-//             borderRadius: '1rem',
-//             display: 'flex',
-//             alignItems: 'center',
-//             justifyContent: 'start',
-//             flexDirection: 'row',
-//             // padding: '20px', // Consistent padding for visual symmetry
-//             position: 'absolute', // Allows exact placement
-//             top: '50%', // Vertically center
-//             left: '10%', // 10% from the left
-//             width: '40%', // Takes up 40% of the container width
-//             transform: 'translateY(-50%)', // Centers the content vertically
-//         },
-        
-//         positions2: {
-//             borderRadius: '1rem',
-//             display: 'flex',
-//             alignItems: 'center',
-//             justifyContent: 'flex-start',
-//             flexDirection: 'row',  
-//             // padding: '20px', // Consistent padding for visual symmetry
-//             position: 'absolute', // Allows exact placement
-//             top: '50%', // Vertically center
-//             right: '10%', // 10% from the right
-//             width: '30%', // Takes up 40% of the container width
-//             transform: 'translateY(-50%)', // Centers the content vertically
-//         },
-
-//         avatar: {
-//             width: '200px',
-//             height: '200px',
-//             borderRadius: '50%',
-//             color: '#bde0fe',
-//             backgroundColor: '#03045e',
-//             display: 'flex',
-//             alignItems: 'center',
-//             justifyContent: 'center',
-//             fontWeight: 'bold',
-//             border: '2px solid #bde0fe',
-//             boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-//             backgroundImage: 'linear-gradient(135deg, #bde0fe 0%, #03045e 100%)',
-//             padding: '1rem',
-//             position: 'relative', // Relative positioning within its container
-//         }
-//     })
-// );
-
-// export default HomePageProfessorStyles;
--- sadFront/src/pages/home/Professor/ProfessorHomePage.tsx
+++ sadFront/src/pages/home/Professor/ProfessorHomePage.tsx
@@ -1,11 +1,15 @@
 import { Avatar, Box, Button, Container, CssBaseline, Grid, Link, TextField, Typography } from "@mui/material";
 
 import HomePageProfessorStyles from "./ProfessorHomePage-styles";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import ProgramCard from "components/programcard/ProgramCard";
 import { ProgramsList } from "components/programslist/ProgramsList";
+import { Stack } from '@mui/material';
 import StudentCard from "components/studentcard/StudentCard";
 import { StudentList } from "components/studentlist";
 import Styles from "Styles";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import clsx from "clsx";
 
 export function ProfessorHomePage() {
@@ -14,6 +18,23 @@ export function ProfessorHomePage() {
 
     return (
         <Box className={ProfessorHomePageClasses.paperContainer}>
+            
+                <Button
+                className={ProfessorHomePageClasses.editprofilebutton}
+                type="submit"
+                // variant="contained"
+                >
+                Edit Profile
+                </Button>
+
+                <Stack
+                className={ProfessorHomePageClasses.socialicons}
+                direction="row" spacing={2}>
+                    <YouTubeIcon style={{ color: '#FF0000', fontSize: 40 }} />
+                    <LinkedInIcon style={{ color: '#0077b5', fontSize: 40 }} />
+                    <TwitterIcon style={{ color: '#1DA1F2', fontSize: 40 }} />
+                </Stack>
+                
                 <Avatar
                 className={ProfessorHomePageClasses.avatar}>
                 </Avatar>
@@ -27,7 +48,8 @@ export function ProfessorHomePage() {
                 </Grid>
                 
 
-                
         </Box>
+    
+        
     );
 }
