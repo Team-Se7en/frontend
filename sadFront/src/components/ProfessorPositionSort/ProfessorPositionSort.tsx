ce79d485df903e1959e615ac43fddf9d45cc8e05
AliAlizadeh <alializadeh.dev11@gmail.com>
2024-04-30 16:55:06 +0330

    feat: add sort own positions and other positions to professor home page

---
+++ sadFront/src/components/ProfessorPositionSort/ProfessorPositionSort.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Theme, useTheme } from '@mui/material/styles';
+
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names: string[] = [
+    'Min Request',
+    'Max Request',
+    'Min Fee',
+    'Max Fee',
+    'Earliest Request Date',
+    'Latest Request Date',
+    'Closest Start date Position',
+    'Farthest Start Date Position',
+];
+
+interface StyleProps {
+    name: string;
+    personName: readonly string[];
+}
+
+function getStyles({ name, personName }: StyleProps, theme: Theme) {
+    return {
+        fontWeight: personName.includes(name) ? theme.typography.fontWeightMedium : theme.typography.fontWeightRegular,
+    };
+}
+
+const ProfessorPositionSort: React.FC = () => {
+    const theme = useTheme();
+    const [personName, setPersonName] = React.useState<string[]>([]);
+
+    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+        const { target: { value } } = event;
+        setPersonName(
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+    const handleDelete = (chipToDelete: string) => {
+        setPersonName((prev) => prev.filter(chip => chip !== chipToDelete));
+    };
+
+    return (
+        <div>
+            <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-chip-label">Sort By</InputLabel>
+                <Select
+                    labelId="demo-multiple-chip-label"
+                    id="demo-multiple-chip"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<OutlinedInput id="select-multiple-chip" label="Sort Criteria" />}
+                    renderValue={(selected: string[]) => (
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                            {selected.map((value) => (
+                                <Chip key={value} label={value} onDelete={() => handleDelete(value)} />
+                            ))}
+                        </Box>
+                    )}
+                    MenuProps={MenuProps}
+                >
+                    {names.map((name) => (
+                        <MenuItem
+                            key={name}
+                            value={name}
+                            style={getStyles({ name, personName }, theme)}
+                        >
+                            {name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </div>
+    );
+}
+
+export default ProfessorPositionSort;
--- sadFront/src/pages/home/Professor/ProfessorHomePage.tsx
+++ sadFront/src/pages/home/Professor/ProfessorHomePage.tsx
@@ -6,7 +6,9 @@ import { Box, CssBaseline } from "@mui/material";
 
 import ProfessorHeader from "../../../components/home_header/ProfessorHeader";
 import ProfessorPositionFilter from "../../../components/ProfessorPositionFilter/ProfessorPositionFilter";
+import ProfessorPositionSort from "../../../components/ProfessorPositionSort/ProfessorPositionSort";
 import ProfessorPositionsFilter from "../../../components/ProfessorPositionsFilter/ProfessorPositionsFilter";
+import ProfessorPositionsSort from "../../../components/ProfessorPositionsSort/ProfessorPositionsSort";
 
 // import ProfessorSort from "../../../components/ProfessorSort/ProfessorSort";
 
@@ -57,16 +59,10 @@ export function ProfessorHomePage() {
             </Box> */}
             {/* <Footer /> */}
 
-            <ProfessorPositionFilter onProfessorFilter={function (filter: IIRFilterOptions): void {
-                throw new Error("Function not implemented.");
-            
-            } }/>
 
-            {/* <ProfessorSort/> */}
+            <ProfessorPositionSort/>
+            <ProfessorPositionsSort/>
 
-            <ProfessorPositionsFilter onProfessorFilter={function (filter: FilterOptions): void {
-                throw new Error("Function not implemented.");
-            } }/>
 
 
         </Box>
