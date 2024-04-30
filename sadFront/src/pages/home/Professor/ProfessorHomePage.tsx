41979b8095f21acc76244caa328bce6bdffbe4d7
AliAlizadeh <alializadeh.dev11@gmail.com>
2024-04-30 18:20:25 +0330

    feat: change items in professor home page

--- sadFront/src/pages/home/Professor/ProfessorHomePage.tsx
+++ sadFront/src/pages/home/Professor/ProfessorHomePage.tsx
@@ -1,7 +1,3 @@
-
-
-// import Styles from "Styles";
-
 import { Box, CssBaseline } from "@mui/material";
 
 import ProfessorHeader from "../../../components/home_header/ProfessorHeader";
@@ -10,59 +6,25 @@ import ProfessorPositionSort from "../../../components/ProfessorPositionSort/Pro
 import ProfessorPositionsFilter from "../../../components/ProfessorPositionsFilter/ProfessorPositionsFilter";
 import ProfessorPositionsSort from "../../../components/ProfessorPositionsSort/ProfessorPositionsSort";
 
-// import ProfessorSort from "../../../components/ProfessorSort/ProfessorSort";
-
-// function showMenu(menu: ProfessorMenus) {
-//     switch (menu) {
-//         case ProfessorMenus.RecentPositions:
-//             return (
-//                 <ProfessorRecentPositions />
-//             );
-//         case ProfessorMenus.Programs:
-//             return (
-//                 <ProgramsList />
-//             );
-
-//         case ProfessorMenus.RecentRequests:
-//             return (
-//                 <StudentList />
-//             )
-
-//         default:
-//             return (
-//                 <></>
-//             )
-//     }
-// }
-
 export function ProfessorHomePage() {
-    // const globalClasses = Styles();
-
-    // const ProfessorHomePageClasses = HomePageProfessorStyles();
-
-    // const [activeMenu, setActiveMenu] = useState(ProfessorMenus.RecentPositions);
-
-    // const handleMenuChange = (menu: ProfessorMenus) => {
-    //     setActiveMenu(menu);
-    // }
 
     return (
         <Box>
             <CssBaseline />
-            {/* <Navbar /> */}
-            {/* <AccountMenu /> */}
+
             <ProfessorHeader/>
-            {/* <Box className={globalClasses.fullyCenter}>
-                {
-                    showMenu(activeMenu)
-                }
-            </Box> */}
-            {/* <Footer /> */}
 
+            <ProfessorPositionFilter onProfessorFilter={function (filter: IIRFilterOptions): void {
+                throw new Error("Function not implemented.");
+            } }/>
 
+            <ProfessorPositionsFilter onProfessorFilter={function (filter: IIRFilterOptions): void {
+                throw new Error("Function not implemented.");
+            } }/>
+            
             <ProfessorPositionSort/>
+            
             <ProfessorPositionsSort/>
-
 
 
         </Box>
