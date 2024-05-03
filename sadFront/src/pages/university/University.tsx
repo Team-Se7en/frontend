363eeb292092b63b3e6eb88e4db46dcd05b3c7b6
Arman <arman.zarkalam@gmail.com>
2024-05-04 00:07:20 +0330

    Adding Uni statistics bar

--- sadFront/src/pages/university/University.tsx
+++ sadFront/src/pages/university/University.tsx
@@ -2,6 +2,9 @@ import Footer from "../../components/footer/footer/footer";
 import Navbar from "../../components/navbar/navbar/navbar";
 import { Box } from "@mui/material";
 import { ProgramsList } from "../../components/programslist/ProgramsList";
+import { Typography } from "@mui/material";
+import { Icon } from "@mui/material";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 export default function UniversityPage() {
   return (
@@ -21,11 +24,123 @@ export default function UniversityPage() {
           flexDirection={"column"}
           width={"100%"}
         >
-          <Box className="uni-image" width={"100%"}>
+          <Box className="uni-image-and-logo" width={"100%"}>
             <img src="src\assets\images\CroppedUniImage.jpg" width={"100%"} />
           </Box>
-          <Box className="uni-logo"></Box>
-          <Box className="statistics"></Box>
+          <Box
+            className="statistics"
+            sx={{ backgroundColor: "#0F1035" }}
+            display={"flex"}
+            flexDirection={"row"}
+            color={"#F2F2F2"}
+            justifyContent={"space-between"}
+            paddingLeft={"4rem"}
+            paddingRight={"4rem"}
+            height={"8rem"}
+            alignItems={"center"}
+          >
+            <Box
+              className="title-and-city"
+              display={"flex"}
+              flexDirection={"column"}
+            >
+              <Typography
+                className="title"
+                variant="h5"
+                sx={{ fontWeight: "bold" }}
+                fontSize={"1.5rem"}
+                color={"#F2F2F2"}
+              >
+                Acadia University
+              </Typography>
+              <Box
+                className="loc-icon-and-tex"
+                display={"flex"}
+                flexDirection={"row"}
+              >
+                <LocationOnIcon></LocationOnIcon>
+                <Typography
+                  className="city-info"
+                  variant="h6"
+                  fontSize={"1rem"}
+                >
+                  Nova Scotia, Canada
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              className="total-students"
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+            >
+              <Typography
+                className="num"
+                variant="h4"
+                fontWeight={"bold"}
+                fontSize={"2rem"}
+              >
+                840
+              </Typography>
+              <Typography className="num" variant="h6" fontSize={"0.8rem"}>
+                Total Student
+              </Typography>
+            </Box>
+            <Box
+              className="international-students"
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+            >
+              <Typography
+                className="num"
+                variant="h4"
+                fontWeight={"bold"}
+                fontSize={"2rem"}
+              >
+                250
+              </Typography>
+              <Typography className="num" variant="h6" fontSize={"0.8rem"}>
+                International Student
+              </Typography>
+            </Box>
+            <Box
+              className="ranking"
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+            >
+              <Typography
+                className="num"
+                variant="h4"
+                fontWeight={"bold"}
+                fontSize={"2rem"}
+              >
+                56
+              </Typography>
+              <Typography className="num" variant="h6" fontSize={"0.8rem"}>
+                Ranking(2024)
+              </Typography>
+            </Box>
+            <Box
+              className="shared-programs"
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+            >
+              <Typography
+                className="num"
+                variant="h4"
+                fontWeight={"bold"}
+                fontSize={"2rem"}
+              >
+                5
+              </Typography>
+              <Typography className="num" variant="h6" fontSize={"0.8rem"}>
+                Total Shared Programs
+              </Typography>
+            </Box>
+          </Box>
         </Box>
         <Box className="bottem-info" display={"flex"} flexDirection={"row"}>
           <Box className="left-side">
