9f3a27afc7f06d533496dcc8d71a238a8d680bf6
Arman <arman.zarkalam@gmail.com>
2024-05-11 20:01:32 +0330

    Fixing Absence of University Page in routing

--- sadFront/src/Routing.tsx
+++ sadFront/src/Routing.tsx
@@ -13,29 +13,37 @@ import { StudentEditProfile } from "./pages/editProfile/StudentEditProfile";
 import StudentHomepage from "./pages/home/Student/StudentHomepage";
 import { StudentSignup } from "./pages/signup/student-signup/StudentSignup";
 import { Verification } from "./pages/forgot-pass/AccountVerification/Verification";
+import UniversityPage from "./pages/university/University";
 
 export default function Routing() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* <Route path="" element={<Home />} /> */}
-                <Route path="login" element={<Login />} />
-                <Route path="signup" element={<Signup />} />
-                <Route path="signup/student" element={<StudentSignup />} />
-                <Route path="signup/professor" element={<ProfessorSignup />} />
-                {/* <Route path="signup/verification" element={<SignupVerfication />} /> */}
-                <Route path="professor/editProfile" element={<ProfessorEditProfile />} />
-                <Route path="student/editProfile" element={<StudentEditProfile />} />
-                <Route path="forgot-pass" element={<Forgot />} />
-                <Route path="verification" element={<Verification />} />
-                <Route path="password/reset/confirm/:uid/:token" element={<Newpassword />} />
-                <Route path="activate/:uid/:token" element={<SignupVerification/>} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="" element={<Home />} /> */}
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="signup/student" element={<StudentSignup />} />
+        <Route path="signup/professor" element={<ProfessorSignup />} />
+        {/* <Route path="signup/verification" element={<SignupVerfication />} /> */}
+        <Route
+          path="professor/editProfile"
+          element={<ProfessorEditProfile />}
+        />
+        <Route path="student/editProfile" element={<StudentEditProfile />} />
+        <Route path="forgot-pass" element={<Forgot />} />
+        <Route path="verification" element={<Verification />} />
+        <Route
+          path="password/reset/confirm/:uid/:token"
+          element={<Newpassword />}
+        />
+        <Route path="activate/:uid/:token" element={<SignupVerification />} />
 
-                <Route path="" element={<Home />} />
-                <Route path="newpassword" element={<Newpassword />} />
-                <Route path="professorhomepage" element={<ProfessorHomePage />} />
-                <Route path="studenthomepage" element={<StudentHomepage />} />
-            </Routes>
-        </BrowserRouter>
-    )
+        <Route path="" element={<Home />} />
+        <Route path="newpassword" element={<Newpassword />} />
+        <Route path="professorhomepage" element={<ProfessorHomePage />} />
+        <Route path="studenthomepage" element={<StudentHomepage />} />
+        <Route path="universitypage" element={<UniversityPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
