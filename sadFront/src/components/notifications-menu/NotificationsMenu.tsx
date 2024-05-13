66b4b576bfec91ce4dd4c795bee232f1c6fe5b8f
Arman <arman.zarkalam@gmail.com>
2024-05-13 16:16:20 +0330

    Adding notification body

--- sadFront/src/components/notifications-menu/NotificationsMenu.tsx
+++ sadFront/src/components/notifications-menu/NotificationsMenu.tsx
@@ -1,18 +1,12 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
-import PersonAdd from "@mui/icons-material/PersonAdd";
-import Settings from "@mui/icons-material/Settings";
-import Logout from "@mui/icons-material/Logout";
-import { Badge } from "@mui/material";
+import { Badge, Button, Divider, Typography } from "@mui/material";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import CampaignIcon from "@mui/icons-material/Campaign";
+import CircleIcon from "@mui/icons-material/Circle";
 
 export default function NotificationsMenu() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -25,7 +19,13 @@ export default function NotificationsMenu() {
   };
   return (
     <React.Fragment>
-      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
         <Tooltip title="Recent Notifications">
           <IconButton
             onClick={handleClick}
@@ -52,6 +52,7 @@ export default function NotificationsMenu() {
           sx: {
             overflow: "visible",
             filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            backgroundColor: "#0F1035",
             mt: 1.5,
             "& .MuiAvatar-root": {
               width: 32,
@@ -76,7 +77,79 @@ export default function NotificationsMenu() {
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <Box width={"15rem"} height={"20rem"}></Box>
+        <Box
+          className="menu-container"
+          width={"22rem"}
+          display={"felx"}
+          flexDirection={"column"}
+          padding={"1rem"}
+        >
+          <Box
+            className="notifs"
+            display={"flex"}
+            flexDirection={"column"}
+            gap={"2rem"}
+            maxHeight={"35rem"}
+            overflow={"auto"}
+          >
+            <Box
+              className="notif-body"
+              display={"flex"}
+              flexDirection={"row"}
+              padding={"0.5rem"}
+              borderBottom={"1px solid gray"}
+            >
+              <Box className="left-main">
+                <Box className="notif-icon-and-text">
+                  <CampaignIcon
+                    sx={{
+                      color: "white",
+                      marginRight: "0.5rem",
+                      marginBottom: "-0.3rem",
+                    }}
+                  />
+                  <Typography
+                    color={"white"}
+                    display={"inline"}
+                    fontSize={"0.85rem"}
+                  >
+                    Xiaodan Xiou opened a new position in Data Science field at
+                    Massachusetts Institute of Technology. You can apply your
+                    request before 16’s November.
+                  </Typography>
+                </Box>
+                <Box className="notif-target-button" textAlign={"right"}>
+                  <Button
+                    variant="text"
+                    sx={{ color: "white", fontSize: "0.8rem" }}
+                  >
+                    View Position
+                  </Button>
+                </Box>
+              </Box>
+              <Box className="right-circle" width={"5rem"} textAlign={"center"}>
+                <Tooltip title="Mark As Seen">
+                  <CircleIcon
+                    sx={{
+                      color: "#4472C4",
+                      fontSize: "1.3rem",
+                      cursor: "pointer",
+                    }}
+                  />
+                </Tooltip>
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            className="all-notifs-button"
+            marginTop={"1rem"}
+            textAlign={"center"}
+          >
+            <Button variant="outlined" sx={{ color: "white" }}>
+              See All Notifications
+            </Button>
+          </Box>
+        </Box>
       </Menu>
     </React.Fragment>
   );
