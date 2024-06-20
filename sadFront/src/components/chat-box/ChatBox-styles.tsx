2662c960c43258ba3c45fb12eceec9458db99870
Arman <arman.zarkalam@gmail.com>
2024-06-20 14:24:14 +0330

    Initializing leftside menu

--- sadFront/src/components/chat-box/ChatBox-styles.tsx
+++ sadFront/src/components/chat-box/ChatBox-styles.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import styled from "styled-components";
 
-export const StyledChats = styled(Box)(({ theme }) => ({
+export const StyledChats = styled(Box)(() => ({
   display: "flex",
   flexDirection: "row",
   //borderBottom: "1px solid gray",
